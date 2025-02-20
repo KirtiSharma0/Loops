1ff3a5a6c671d0f7d42564bbdaa1b499c504d024
Kirti Sharma <128890117+KirtiSharma0@users.noreply.github.com>
2025-02-20 16:38:02 +0530

    Add files via upload

---
+++ for5table.cpp
@@ -0,0 +1,11 @@
+//print table of any number taking input
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter value of n = ";
+    cin>>n;
+    for(int i=1;i<=10;i++){
+        cout<<n*i<<endl;
+    }
+}
