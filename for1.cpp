68061faa1fba9bc9f7c78be4674eebd093d4385d
Kirti Sharma <128890117+KirtiSharma0@users.noreply.github.com>
2025-02-20 16:32:26 +0530

    Add files via upload

---
+++ for1.cpp
@@ -0,0 +1,10 @@
+//10 times good morning
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=1;i<=10;i++){
+        cout<<"good morning"<<endl;
+        cout<<"hello"<<endl;
+    }
+    return 0;
+}
---
+++ for2.cpp
@@ -0,0 +1,11 @@
+//print anything n times taking input
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cout<<"My doll is pretty";
+    }
+    return 0;
+}
